7547abf22f5dd310def65e517d84c2717552c070
Hima <abrahimmahmoud5454@gmail.com>
2023-10-05 01:54:35 +0300

    si

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+#def uppercase(str):
+def uppercase(str):
+        for c in str:
+                if ord(c) >= 65 and ord(c) <= 90:
+                        return(True)
+                else:
+                        return(False)
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
