639fc1cf05b6417a25dccbfcf58e2ee850ca4235
Hima <abrahimmahmoud5454@gmail.com>
2023-10-04 23:07:44 +0300

    si

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-  if i != 101 and i != 113:
-    print(chr(i), end="")
+def print_alphabt():
+  for i in range(97, 123):
+    if i != 101 and i != 113:
+      print(chr(i), end="")
+
+print_alphabt()
