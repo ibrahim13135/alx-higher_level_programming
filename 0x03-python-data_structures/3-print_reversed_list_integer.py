0b9554c4947e6fca39fe104cf9b0d2d40f3f10db
Hima <abrahimmahmoud5454@gmail.com>
2023-10-10 05:09:27 +0300

    si

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,8 +3,3 @@
 def print_reversed_list_integer(my_list=[]):
     for s in range(len(my_list) -1, -1, -1):
         print("{}".format(my_list[s]))
-
-
-
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 4-new_in_list.py
-
 
 def new_in_list(my_list, idx, element):
     """Replace an element in a copied list at a specific position."""
@@ -10,13 +8,3 @@ def new_in_list(my_list, idx, element):
     copy = [x for x in my_list]
     copy[idx] = element
     return (copy)
-
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
