9b3c9057f313edbdb049981ba22f67482837c991
Hima <asondos867@gmail.com>
2024-02-13 02:30:45 +0200

    si

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 if (Number.isInteger(parseInt(process.argv[2]))) {
-    let x = parseInt(process.argv[2]);
-    for (var i = 0; i < x; i++) {
-      console.log("C is fun");
-    }
-  } else {
-    console.log("Missing number of occurrences");
+  let x = parseInt(process.argv[2]);
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
+}
