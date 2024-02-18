9cc5d06bdf4dcbb9028d6b019baa105dea6df1ac
Hima <asondos867@gmail.com>
2024-02-19 01:26:32 +0200

    si

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Lists cities in California"""
+"""Lists all cities"""
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <db_name>")
+        sys.exit(1)
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
-                "JOIN states ON cities.state_id = states.id "
-                "WHERE states.name = 'California' "
-                "ORDER BY cities.id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                 JOIN states ON cities.state_id = states.id \
+                 ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
